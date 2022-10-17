2ed6879c90c0be527f3135e2c5a7af66e5f13073
Semion <semion.probochkin@yandex.ru>
2022-10-17 15:12:43 +0300

    Состояние нити

--- src/main/java/ru/concurrent/ConcurrentOutput.java
+++ src/main/java/ru/concurrent/ConcurrentOutput.java
@@ -1,15 +1,27 @@
 package ru.concurrent;
 
 public class ConcurrentOutput {
+
     public static void main(String[] args) {
+        another();
+        second();
+        System.out.println("работа завершена");
+    }
+    public static void another() {
         Thread another = new Thread(
                 () -> System.out.println(Thread.currentThread().getName())
         );
         another.start();
+        while (another.getState() != Thread.State.TERMINATED) {
+        }
+    }
+    public static void second() {
         Thread second = new Thread(
                 () -> System.out.println(Thread.currentThread().getName())
         );
         second.start();
-        System.out.println(Thread.currentThread().getName());
+        while (second.getState() != Thread.State.TERMINATED) {
+
+        }
     }
 }
