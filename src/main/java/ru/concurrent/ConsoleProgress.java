6b8e8e12b023b2c55d2f32f144aa01be0febd3f0
Semion <semion.probochkin@yandex.ru>
2023-01-13 17:27:50 +0300

    Прерывание нити

---
+++ src/main/java/ru/concurrent/ConsoleProgress.java
@@ -0,0 +1,34 @@
+package ru.concurrent;
+
+
+public class ConsoleProgress implements Runnable {
+    @Override
+    public void run() {
+        try {
+        var process = new char[] {'-', '\\', '|', '/'};
+        int i = 0;
+        while (!Thread.currentThread().isInterrupted()) {
+
+            if (i > process.length -1) {
+                i = 0;
+            }
+            System.out.print("\r load: " + process[i]);
+            i++;
+            Thread.sleep(500);
+        }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+
+    }
+    public static void main(String[] args)  throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        progress.sleep(5000); /* симулируем выполнение параллельной задачи в течение 5 секунд. */
+        progress.interrupt();
+    }
+
+
+
+}
