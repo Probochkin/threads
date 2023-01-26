b0a2a5c1d604ebed75b48f2993520aefe91a3ca1
Semion <semion.probochkin@yandex.ru>
2023-01-26 21:17:13 +0300

    Синхронизация общих ресурсов

---
+++ src/main/java/ru/sharedresources/Cache.java
@@ -0,0 +1,12 @@
+package ru.sharedresources;
+
+public class Cache {
+    private static Cache cache;
+
+    public synchronized static Cache instOf() {
+        if (cache == null) {
+            cache = new Cache();
+        }
+        return cache;
+    }
+}
