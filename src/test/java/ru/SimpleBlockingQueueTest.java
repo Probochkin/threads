0308ed73bf79bf2a443cb19fef1f7c14e5c5002d
Semion <semion.probochkin@yandex.ru>
2023-05-03 10:44:25 +0300

    Junit тест для блокирующей очереди

--- src/test/java/ru/SimpleBlockingQueueTest.java
+++ src/test/java/ru/SimpleBlockingQueueTest.java
@@ -2,35 +2,46 @@ package ru;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
+
+import java.util.Arrays;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.IntStream;
 import static org.assertj.core.api.Assertions.*;
+
 
 class SimpleBlockingQueueTest {
     @Test
-    void whenPollThenOffer() throws InterruptedException {
-        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
-        List<Integer> values = new ArrayList<>();
-        Thread producer = new Thread(() -> {
-            try {
-                queue.offer(123);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-                Thread.currentThread().interrupt();
-            }
-        });
-        Thread consumer = new Thread(() -> {
-            try {
-                values.add(queue.poll());
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-                Thread.currentThread().interrupt();
-            }
-        });
+    public void whenFetchAllThenGetIt() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
+        Thread producer = new Thread(
+                () -> {
+                    IntStream.range(0, 5).forEach(i -> {
+                        try {
+                            queue.offer(i);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    });
+                }
+        );
         producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.poll());
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
         consumer.start();
         producer.join();
+        consumer.interrupt();
         consumer.join();
-        assertThat(values.get(0)).isEqualTo(123);
+        assertThat(buffer).isEqualTo(Arrays.asList(0, 1, 2, 3, 4));
     }
 }
